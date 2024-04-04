9f3efd6269b8ab77bc148a43b3d1eb66eb5d8b72
jrtchan <136003794+jrtchan@users.noreply.github.com>
2024-04-04 13:38:28 +0800

    Create advanced.py

---
+++ assignment_templates/advanced.py
@@ -0,0 +1,163 @@
+'''Python: Advanced
+
+70 points
+
+This assignment will develop your ability to manipulate data.
+We expect that this assignment will equip you to understand
+    Python tutorials.
+
+Please refer to the file `advanced_sample_data.py` for examples of:
+- the `social_graph` parameter for the relationship_status item
+- the `board` parameter for the tic_tac_toe item
+- the `route_map` parameter for the eta item
+'''
+
+def relationship_status(from_member, to_member, social_graph):
+    '''Relationship Status.
+    20 points.
+
+    Let us pretend that you are building a new app.
+    Your app supports social media functionality, which means that users can have
+    relationships with other users.
+
+    There are two guidelines for describing relationships on this social media app:
+    1. Any user can follow any other user.
+    2. If two users follow each other, they are considered friends.
+
+    This function describes the relationship that two users have with each other.
+
+    Please see `advanced_sample_data.py` for sample data. The social graph
+    will adhere to the same pattern.
+
+    Parameters
+    ----------
+    from_member: str
+        the subject member
+    to_member: str
+        the object member
+    social_graph: dict
+        the relationship data
+
+    Returns
+    -------
+    str
+        "follower" if from_member follows to_member,
+        "followed by" if from_member is followed by to_member,
+        "friends" if from_member and to_member follow each other,
+        "no relationship" if neither from_member nor to_member follow each other.
+    '''
+    # Replace `pass` with your code.
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    follower = to_member in social_graph[from_member]["following"]
+    followed_by = from_member in social_graph[to_member]["following"]
+    
+    if follower and followed_by == True:
+        return 'friends'
+    
+    elif follower == True:
+        return 'follower'
+    
+    elif followed_by == True:
+        return 'followed by'
+    
+    else:
+        return 'no relationship'
+
+def tic_tac_toe(board):
+    '''Tic Tac Toe.
+    25 points.
+
+    Tic Tac Toe is a common paper-and-pencil game.
+    Players must attempt to successfully draw a straight line of their symbol across a grid.
+    The player that does this first is considered the winner.
+
+    This function evaluates a tic tac toe board and returns the winner.
+
+    Please see `advanced_sample_data.py` for sample data. The board will adhere
+    to the same pattern. The board may by 3x3, 4x4, 5x5, or 6x6. The board will never
+    have more than one winner. The board will only ever have 2 unique symbols at the same time.
+
+    Parameters
+    ----------
+    board: list
+        the representation of the tic-tac-toe board as a square list of lists
+
+    Returns
+    -------
+    str
+        the symbol of the winner, or "NO WINNER" if there is no winner
+    '''
+    # Replace `pass` with your code.
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    def winning_line(line):
+        if len(set(line)) == 1 and line[0]:
+            return line[0]
+
+    for row in board:
+        winner = winning_line(row)
+        if winner:
+            return winner
+
+    for col in range(len(board)):
+        column = [board[row][col] for row in range(len(board))]
+        winner = winning_line(column)
+        if winner:
+            return winner
+
+    top_left_diagonal = [board[x][x] for x in range(len(board))]
+    winner = winning_line(top_left_diagonal)
+    if winner:    
+        return winner
+
+    bottom_left_diagonal = [board[x][(-1)*(x+1)] for x in range(len(board))]
+    winner = winning_line(bottom_left_diagonal)
+    if winner:
+        return winner
+
+    return "NO WINNER"
+
+def eta(first_stop, second_stop, route_map):
+    '''ETA.
+    25 points.
+
+    A shuttle van service is tasked to travel along a predefined circlar route.
+    This route is divided into several legs between stops.
+    The route is one-way only, and it is fully connected to itself.
+
+    This function returns how long it will take the shuttle to arrive at a stop
+    after leaving another stop.
+
+    Please see `advanced_sample_data.py` for sample data. The route map will
+    adhere to the same pattern. The route map may contain more legs and more stops,
+    but it will always be one-way and fully enclosed.
+
+    Parameters
+    ----------
+    first_stop: str
+        the stop that the shuttle will leave
+    second_stop: str
+        the stop that the shuttle will arrive at
+    route_map: dict
+        the data describing the routes
+
+    Returns
+    -------
+    int
+        the time it will take the shuttle to travel from first_stop to second_stop
+    '''
+    # Replace `pass` with your code.
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    current_stop = first_stop
+    total_travel_time = 0
+    
+    while current_stop != second_stop:
+        next_stop = None
+        for leg in route_map:
+            if leg[0] == current_stop:
+                next_stop = leg[1]
+                total_travel_time += route_map[leg]['travel_time_mins']
+                break
+        
+        current_stop = next_stop
+    
+    return total_travel_time
