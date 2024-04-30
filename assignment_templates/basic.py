dbb74188b9c3a1d2dbd98cc53ff4677d2d907e51
jrtchan <136003794+jrtchan@users.noreply.github.com>
2024-04-30 16:26:56 +0800

    Update basic.py

--- assignment_templates/basic.py
+++ assignment_templates/basic.py
@@ -37,3 +37,73 @@ def savings(gross_pay, tax_rate, expenses):
     after_tax = math.floor(gross_pay - (tax_rate*gross_pay))
     after_tax_expenses = after_tax - expenses
     return after_tax_expenses
+
+def material_waste(total_material, material_units, num_jobs, job_consumption):
+    '''Material Waste.
+    5 points.
+
+    This function calculates how much material input will be wasted
+        after running a certain number of jobs that consume
+        a set amount of material.
+
+    To get the waste of a set of jobs:
+        1. Multiply the number of jobs by the material consumption per job.
+        2. Subtract the total material consumed from the total material available.
+
+    The users of this function also want you to format the output as a string, annotated with the
+        units in which the material is expressed. Do not add a space between the number and the unit.
+
+    Parameters
+    ----------
+    total_material: int
+        the total material available
+    material_units: str
+        the units used to express a quantity of the material (e.g., "kg", "L", etc.)
+    num_jobs: int
+        the number of jobs to run
+    job_consumption: int
+        the amount of material consumed per job
+
+    Returns
+    -------
+    str
+        the amount of remaining material expressed with its unit (e.g., "10kg").
+    '''
+    # Replace `pass` with your code.
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    total_consumption = num_jobs * job_consumption
+    waste = str(total_material - total_consumption) + material_units
+    return waste
+
+def interest(principal, rate, periods):
+    '''Interest.
+    5 points.
+
+    This function calculates the final value of an investment after
+        gaining simple interest over a number of periods.
+
+    To calculate simple interest, simply multiply the principal to the quantity (rate * time).
+        Add this amount to the principal to get the final value.
+
+    Round down the final amount.
+
+    Parameters
+    ----------
+    principal: int
+        the principal (i.e., starting) amount invested, expressed in centavos
+    rate: float
+        the interest rate per period, expressed as a decimal representation of a percentage (e.g., 3% is 0.03)
+    periods: int
+        the number of periods invested
+
+    Returns
+    -------
+    int
+        the final value of the investment
+    '''
+    # Replace `pass` with your code.
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    import math
+    additional = principal * rate * periods
+    total = math.floor(principal + additional)
+    return total
